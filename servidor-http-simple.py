2ce92dc835f434f13b264d7019c58605e53d4e3b
Borja Egea Madrid <boegma@hotmail.com>
2016-02-16 01:57:26 +0100

    Update servidor-http-simple.py
    He hecho el Ejercicio 14.1 (Servidor Simple)

--- servidor-http-simple.py
+++ servidor-http-simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# Borja Egea Madrid
+
 """
 Simple HTTP Server
 Jesus M. Gonzalez-Barahona and Gregorio Robles
@@ -9,28 +11,24 @@ TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
 
 import socket
 
-# Create a TCP objet socket and bind it to a port
-# We bind to 'localhost', therefore only accepts connections from the
-# same machine
-# Port should be 80, but since it needs root privileges,
-# let's use one above 1024
-
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind(('localhost', 1234))
+mySocket.bind(('localhost', 32122))
 
 # Queue a maximum of 5 TCP connection requests
 
 mySocket.listen(5)
 
-# Accept connections, read incoming data, and answer back an HTML page
-#  (in an infinite loop)
+# La IP esta alojada en la posicion 1 de address
+# El Puerto esta alojado en la possicion 2 de address
 
 while True:
-    print 'Waiting for connections'
-    (recvSocket, address) = mySocket.accept()
-    print 'HTTP request received:'
-    print recvSocket.recv(1024)
-    recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
-                    "<html><body><h1>Hello World!</h1></body></html>" +
-                    "\r\n")
-    recvSocket.close()
+	print 'Waiting for connections'
+	(recvSocket, address) = mySocket.accept()
+	print 'HTTP request received:'
+	print recvSocket.recv(1024)
+	recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
+			"<html><body><h1>Hola! Eres de esta IP: " + address[0] +
+			" y de este Puerto: " + str(address[1]) + 
+			"</h1></body></html>" +
+			"\r\n")
+	recvSocket.close()
